4be882c56213a6b13c73d3fc43a7b131c00ccab9
MarvinJWendt <git@marvinjwendt.com>
2021-04-15 15:31:17 +0200

    feat(snippets): added commit.js

---
+++ commit.js
@@ -0,0 +1,58 @@
+const exec = require('child_process').execSync
+
+run("git add .")
+
+let changedFiles = run("git status -s").split("\n").map(e => e.split(" ")[e.split(" ").length - 1])
+
+let files = []
+
+for (const changedFile of changedFiles) {
+    const args = changedFile.split("/")
+    let type = ""
+    let message = ""
+    if (args.length === 1) {
+        const name = args[0]
+        args[0] = "snippets"
+        args.push(name)
+    }
+
+    const status = run(`git status ${changedFile} -s`).split("")[0]
+
+    switch (status) {
+        case "A":
+            type = "feat"
+            message = "added"
+            break
+        case "M":
+            type = "refactor"
+            message = "modified"
+            break
+        case "D":
+            type = "refactor"
+            message = "deleted"
+            break;
+        default:
+            break;
+    }
+
+    files.push({
+        path: changedFile,
+        name: args[0] === "snippets" ? args[args.length - 1] : args[args.length - 2],
+        language: args[0],
+        commitType: type,
+        commitMessage: message
+    })
+}
+
+for (let file of files) {
+    console.log(run(`git restore --staged ${file.path}`))
+}
+
+for (let file of files) {
+    console.log(run(`git add ${file.path}`))
+    console.log(run(`git commit -m "${file.commitType}(${file.language}): ${file.commitMessage} ${file.name}"`))
+}
+
+function run(cmd) {
+    return exec(cmd, {encoding: 'utf-8'}).trim()
+}
