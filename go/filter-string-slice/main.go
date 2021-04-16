e1a8517f80b7437d4ff5e52ff8881a258308e605
MarvinJWendt <git@marvinjwendt.com>
2021-04-16 10:53:45 +0000

    go: added main.go in filter-string-slice

---
+++ go/filter-string-slice/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	unfiltered := []string{"one", "two", "three", "four", "five", "six"}
+
+	filtered := FilterStringSlice(unfiltered, func(x string) bool {
+		return len(x) <= 3 // Filter words which have 3 or less characters.
+	})
+
+	fmt.Printf("Unfiltered: %v\n", unfiltered)
+	fmt.Printf("Filtered:   %v\n", filtered)
+}
+
+// FilterStringSlice filters a string slice based on a predicate
+func FilterStringSlice(s []string, keep func(x string) bool) []string {
+	if len(s) == 0 {
+		return s
+	}
+
+	var ret []string
+	for _, v := range s {
+		if keep(v) {
+			ret = append(ret, v)
+		}
+	}
+
+	return ret
+}
