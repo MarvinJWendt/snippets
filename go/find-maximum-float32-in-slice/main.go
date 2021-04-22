cb9cdac8f52ab893e632c40ebb8b8f64d9bdf66b
MarvinJWendt <git@marvinjwendt.com>
2021-04-22 14:47:30 +0200

    go: added main.go in find-maximum-float32-in-slice

---
+++ go/find-maximum-float32-in-slice/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var s []float32
+	s = []float32{0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1}
+	fmt.Println(FindMaximumFloat32InSlice(s)) // Output: 0.9
+
+	s = []float32{0.5, 0.4, 0.3, 0.2, 0.9, 0.5, 0.8, 0.3, 0.6, 0.7, -0.1, 0.7, 0.8, 0.3, 0.4, 0.6, 0.2, 0.3}
+	fmt.Println(FindMaximumFloat32InSlice(s)) // Output: -0.9
+
+	s = []float32{-0.1, -0.2, -0.3, -0.1337, -0.4, -0.5, -0.6, -0.7, -0.8, -0.9}
+	fmt.Println(FindMaximumFloat32InSlice(s)) // Output: -0.1
+}
+
+// FindMaximumFloat32InSlice finds the maximum of an float32 slice
+func FindMaximumFloat32InSlice(s []float32) float32 {
+	var max float32
+	for i, v := range s {
+		if i == 0 || v > max {
+			max = v
+		}
+	}
+	return max
+}
